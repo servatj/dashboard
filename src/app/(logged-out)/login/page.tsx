9e5e21767c9c523cda2bf7ad6bc0b81afe19f973
servatj <jservatlorca@gmail.com>
2024-03-24 13:05:48 +0400

    signup form

--- src/app/(logged-out)/login/page.tsx
+++ src/app/(logged-out)/login/page.tsx
@@ -103,7 +103,7 @@ export default function LoginPage() {
 				</Form>
 			</CardContent>
 			<CardFooter className="justify-between">
-				<small>Don t have an account?</small>
+				<small>Do you have an account?</small>
 				<Button asChild variant="outline" size="sm">
 					<Link href="/sign-up">Sign Up</Link>
 				</Button>
